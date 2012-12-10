854ca579890a83fb288e7b3ff77271b5c22943d7
nicko <nicko466@gmail.com>
2012-12-10 17:00:01 +0100

    Tentative de commit de merge

--- ihmGraphe/src/um/Main.java
+++ ihmGraphe/src/um/Main.java
@@ -4,16 +4,24 @@
  */
 package um;
 
+import ui.*;
+
+
+
+
 /**
  *
  * @author nicko2
  */
 public class Main {
-
+    
+    
+    static Graphe graphe;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        graphe = new Graphe();
+        new mainWindow();
     }
 }
--- ihmGraphe/src/um/Noeud.java
+++ ihmGraphe/src/um/Noeud.java
@@ -5,9 +5,7 @@ import java.awt.Point;
 public class Noeud {
 
   private Type type;
-
   public String name;
-
   public Point position;
 
   public void noeud(Type type, String name) {
