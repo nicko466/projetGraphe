cb1d294960082c5837258e3dbf44589f71ed1231
Bigoud <bigoud@pc-Bigoud.(none)>
2012-12-10 16:52:29 +0100

    blablabla

--- ihmGraphe/src/um/Graphe.java
+++ ihmGraphe/src/um/Graphe.java
@@ -1,16 +1,18 @@
 package um;
 
 import java.awt.Point;
+import java.util.Iterator;
 import java.util.List;
 
 public class Graphe {
 
-  public List sommets;
-  public List aretes;
+  public List<Noeud> sommets;
+  public List<Arete> aretes;
   public Noeud racine;
   public Noeud courant;
   
-  static final int scale = 12;
+  static final int scaleY = 12;
+  static final int scaleX = 12;
 
   public void Graphe() {
         racine = new Noeud(Type.User, "Task_"+sommets.size(), 0, new Point(0,0));
@@ -19,7 +21,7 @@ public class Graphe {
   }
 
   public void add(Type type) {
-        Noeud noeud = new Noeud(type, "Task_"+sommets.size(), courant.getLevel()+1, new Point(courant.getPosition().x, courant.getPosition().y + scale));
+        Noeud noeud = new Noeud(type, "Task_"+sommets.size(), courant.getLevel()+1, new Point(courant.getPosition().x, courant.getPosition().y + scaleY));
         sommets.add(noeud);
         aretes.add(new Arete(courant, noeud));//attention aux références
         courant = noeud;
@@ -27,5 +29,13 @@ public class Graphe {
 
   public void delete(int arg2) {
   }
-
+  
+  private int countLevel(List<Noeud> listeNoeud, int level){
+      int count = 0;
+      Iterator<Noeud> it = listeNoeud.iterator();
+      while(it.hasNext()){
+          if(it.next().getLevel()==level)count++;
+      }
+      return count;
+  }
 }
