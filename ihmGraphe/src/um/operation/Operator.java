4405d790fb9b1f6ccedd3616fd2701b3aa6b48cc
Bigoud <bigoud@pc-Bigoud.(none)>
2012-12-24 18:42:39 +0100

    ajout de la liste op dans graphe, modif des construc d'op, reimplementation d'add et delete

--- ihmGraphe/src/um/operation/Operator.java
+++ ihmGraphe/src/um/operation/Operator.java
@@ -1,15 +1,21 @@
 package um.operation;
 
+import um.Graphe;
 import um.Noeud;
 
 
 public class Operator{
     private String name;
     private Noeud n1;
+
+    public Noeud getN1() {
+        return n1;
+    }
     
     public Operator(String name, Noeud n1){
         this.name = name;
         this.n1 = n1;
+        Graphe.op.add(this);
     }
   
 
